89a5886a398ad25a2f57479e274883ea6d280c98
Tereza_Vrbova1 <tereza.vrbova1@tul.cz>
2024-03-31 22:17:29 +0200

    Update api key settings

--- src/main/java/cz/stin/controller/RequestController.java
+++ src/main/java/cz/stin/controller/RequestController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RequestController {
 
+    private String apiKey = System.getenv("API_KEY");
+
     @RequestMapping("/api")
     public String hello() {
         return "Hello world";
