7ebcd2994296d4d81726d883ff80bda07fe79454
Tereza_Vrbova1 <tereza.vrbova1@tul.cz>
2024-03-31 19:07:05 +0200

    Setup Hello world tests

---
+++ src/test/java/cz/stin/StinApplicationTest.java
@@ -0,0 +1,13 @@
+package cz.stin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
