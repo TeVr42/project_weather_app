8c15ecefd06a10ec25948788d9fa2711f764e3fa
Tereza_Vrbova1 <tereza.vrbova1@tul.cz>
2024-04-14 12:34:12 +0200

    Add navbar and refactoring

--- src/main/java/cz/stin/service/WeatherAPIService.java
+++ src/main/java/cz/stin/service/WeatherAPIService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
@@ -27,10 +28,6 @@ public class WeatherAPIService {
         return getWeatherData(location, "forecast");
     }
 
-    public String getForecastWeather(String location, String date) {
-        return getWeatherData(location, "forecast", date);
-    }
-
     public String getHistoricalWeather(String location) {
         LocalDate currentDate = LocalDate.now();
         LocalDate fiveDaysAgoDate = currentDate.minusDays(5);
@@ -48,35 +45,55 @@ public class WeatherAPIService {
         return getWeatherData(location, weatherType, null);
     }
 
-    private String getWeatherData(String location, String weatherType, String date) {
-        UriComponentsBuilder builder = UriComponentsBuilder.fromUriString("http://api.weatherapi.com/v1/{weatherType}.json")
-                .queryParam("key", apiKey).queryParam("q", location).queryParam("lang","cs");
-
-        if (date != null) {
-            builder.queryParam("dt", date);
-        }
-
-        if (weatherType.equals("history")) {
-            LocalDate currentDate = LocalDate.now();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-            String formattedCurrentDate = currentDate.format(formatter);
-            builder.queryParam("end_dt", formattedCurrentDate);
-
-            if (date == null) {
-                throw new IllegalArgumentException("Date parameter is required for historical weather");
-            }
-        }
-
-        if (weatherType.equals("forecast")) {
-            builder.queryParam("days", 3);
-        }
-
-        ResponseEntity<String> response = restTemplate.getForEntity(builder.buildAndExpand(weatherType).toUri(), String.class);
-
+    private String getResponse(URI url) {
+        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
         if (response.getStatusCode().is2xxSuccessful()) {
             return response.getBody();
         } else {
             throw new RuntimeException("HTTP request failed with error code: " + response.getStatusCodeValue());
         }
     }
+
+    private String getWeatherData(String location, String weatherType, String date) {
+        UriComponentsBuilder builder = buildBaseUri(location);
+
+        switch (weatherType) {
+            case "history":
+                handleHistory(builder, date);
+                break;
+            case "forecast":
+                handleForecast(builder);
+                break;
+            case "current":
+                break;
+            default:
+                throw new IllegalArgumentException("Invalid weather type");
+        }
+
+        return getResponse(builder.buildAndExpand(weatherType).toUri());
+    }
+
+    private UriComponentsBuilder buildBaseUri(String location) {
+        return UriComponentsBuilder.fromUriString("http://api.weatherapi.com/v1/{weatherType}.json")
+                .queryParam("key", apiKey)
+                .queryParam("q", location)
+                .queryParam("lang", "cs");
+    }
+
+    private void handleHistory(UriComponentsBuilder builder, String date) {
+        if (date == null) {
+            throw new IllegalArgumentException("Date parameter is required for historical weather");
+        } else {
+            builder.queryParam("dt", date);
+        }
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        String formattedCurrentDate = currentDate.format(formatter);
+        builder.queryParam("end_dt", formattedCurrentDate);
+    }
+    private void handleForecast(UriComponentsBuilder builder) {
+        builder.queryParam("days", 3);
+    }
+
 }
